8a30609d643cae884364e158e1db5ee96e4ab4ca
hemantgokhale <hemantgokhale@gmail.com>
2025-02-11 22:40:27 -0800

    1. Switched from material to material3 2. LystaList - WIP

--- composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/App.kt
+++ composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/App.kt
@@ -1,37 +1,35 @@
 package dev.hgokhale.lysta
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import lysta.composeapp.generated.resources.Res
-import lysta.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+        Column(
+            Modifier.fillMaxWidth().padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            val list = remember {
+                LystaList(
+                    "Groceries",
+                    listOf(
+                        LystaList.Item("Milk", false),
+                        LystaList.Item("Eggs", false),
+                        LystaList.Item("Bread", true),
+                        LystaList.Item("Butter", true),
+                    )
+                )
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            LystaList(list)
         }
     }
 }
