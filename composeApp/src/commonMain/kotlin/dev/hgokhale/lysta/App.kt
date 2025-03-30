50b434f438594de2a81ac57101272f0c31e507d3
hemantgokhale <hemantgokhale@gmail.com>
2025-03-30 14:11:46 -0700

    DraggableFAB

--- composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/App.kt
+++ composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/App.kt
@@ -1,11 +1,36 @@
 package dev.hgokhale.lysta
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun App() {
+fun App(viewModel: LystViewModel = viewModel { LystViewModel() }) {
     MaterialTheme {
-        Home()
+        // We add this colum to center the app content on a large screen e.g. desktop or web
+        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Scaffold(
+                modifier = Modifier.widthIn(max = 450.dp),
+                topBar = { TopAppBar(title = { Text("Lists") }) },
+                floatingActionButton = { DraggableFAB { println("FAB Clicked") } },
+            ) { paddingValues ->
+                Home(
+                    modifier = Modifier.padding(paddingValues),
+                    viewModel = viewModel
+                )
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/Home.kt
+++ composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/Home.kt
@@ -1,9 +1,9 @@
 package dev.hgokhale.lysta
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
@@ -11,12 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun Home(modifier: Modifier = Modifier, viewModel: LystViewModel = viewModel { LystViewModel() }) {
-    LazyColumn(modifier = modifier.widthIn(max = 450.dp).padding(16.dp)) {
-
+fun Home(modifier: Modifier = Modifier, viewModel: LystViewModel) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
         items(
             items = viewModel.lists,
             key = { item -> item.id }
