83a4764d036626de40150d7d279947e4a1a1e08a
hemantgokhale <hemantgokhale@gmail.com>
2025-04-21 08:19:21 -0700

    Swipe-to-delete a list

--- composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/hgokhale/lysta/HomeScreen.kt
@@ -1,6 +1,9 @@
 package dev.hgokhale.lysta
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,15 +13,17 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -42,21 +47,55 @@ private fun Home(modifier: Modifier = Modifier, viewModel: LystViewModel) {
             items = lists,
             key = { item -> item.id }
         ) { item ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = item.name.value,
-                    modifier = Modifier
-                        .weight(1f)
-                        .clickable { viewModel.onListClicked(item.id) }
-                )
-                IconButton(onClick = { viewModel.deleteList(item.id) }) {
-                    Icon(painter = rememberVectorPainter(image = Icons.Default.Delete), contentDescription = "Delete", tint = Color.Black)
-                }
+            DismissibleItem(viewModel, item)
+        }
+    }
+}
+
+@Composable
+private fun DismissibleItem(viewModel: LystViewModel, item: Lyst) {
+    val dismissState = rememberSwipeToDismissBoxState(
+        confirmValueChange = { value ->
+            if (value == SwipeToDismissBoxValue.EndToStart) {
+                viewModel.deleteList(item.id)
+                true
+            } else {
+                false
             }
+        }
+    )
+    SwipeToDismissBox(
+        state = dismissState,
+        backgroundContent = {
+            val color by
+            animateColorAsState(
+                when (dismissState.targetValue) {
+                    SwipeToDismissBoxValue.Settled -> MaterialTheme.colorScheme.background
+                    SwipeToDismissBoxValue.StartToEnd -> Color.Red
+                    SwipeToDismissBoxValue.EndToStart -> Color.Red
+                }
+            )
+            Box(Modifier.fillMaxSize().background(color)) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete",
+                    tint = Color.White
+                )
+            }
+        },
+        enableDismissFromStartToEnd = false
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = item.name.value,
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable { viewModel.onListClicked(item.id) }
+            )
         }
     }
 }
